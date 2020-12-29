33706bb59889686ca8083e7f99db027eeb533555
ManishRaghavan <manish.mahendran9499@gmail.com>
2020-12-30 00:01:15 +0530

    start page adding header

--- Header/header.js
+++ Header/header.js
@@ -1,7 +1,6 @@
 const search = document.getElementById('header__social__search')
 const btn = document.getElementById('header__social__search_btn')
 const input = document.getElementById('header__social__search_input')
-console.log(btn, search)
 btn.addEventListener('click', () => {
     search.classList.toggle('active')
     
