88324ee17e428b4aabed348e7f28071c6deb34e0
masai school <rabiabasari7@gmail.com>
2020-12-31 21:58:16 +0530

    login_page

---
+++ login_page/header.js
@@ -0,0 +1,8 @@
+const search = document.getElementById('header__social__search')
+const btn = document.getElementById('header__social__search_btn')
+const input = document.getElementById('header__social__search_input')
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    
+    input.focus()
+})
